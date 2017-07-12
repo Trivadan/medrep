9de29a83c815bcc0cf3f791333c2495f5e4b59f4
Trivadan <danstanochieng@gmail.com>
2017-07-12 10:43:00 +0300

    Update all the files online

---
+++ js/main.js
@@ -0,0 +1,36 @@
+//Datepicker JQuery
+$( document ).ready(function() {
+  $('#date-from').datepicker({
+    format: 'mm-dd-yyyy',
+  });
+  $('#date-to').datepicker();   
+  
+  $('.date-picker').datepicker().on('changeDate',function(e) {
+  $('.date-picker').datepicker('hide');
+  });
+        
+// disabling dates
+var nowTemp = new Date();
+var now = new Date(nowTemp.getFullYear(), nowTemp.getMonth(), nowTemp.getDate(), 0, 0, 0, 0);
+
+var checkin = $('#date-from').datepicker({
+  onRender: function(date) {
+  return date.valueOf() < now.valueOf() ? 'disabled' : '';
+  }
+}).on('changeDate', function(ev) {
+  if (ev.date.valueOf() > checkout.date.valueOf()) {
+  var newDate = new Date(ev.date)
+  newDate.setDate(newDate.getDate() + 1);
+  checkout.setValue(newDate);
+  }
+  checkin.hide();
+  $('#date-to')[0].focus();
+}).data('datepicker');
+var checkout = $('#dpd2').datepicker({
+  onRender: function(date) {
+  return date.valueOf() <= checkin.date.valueOf() ? 'disabled' : '';
+  }
+}).on('changeDate', function(ev) {
+  checkout.hide();
+}).data('datepicker');
+});
